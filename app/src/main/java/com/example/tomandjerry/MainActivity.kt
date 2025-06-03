64439740f52f420a827a5d9f4f2c34b0e1983534
Thoraya <thoriahamdy99@gmail.com>
2025-06-03 03:45:46 +0300

    call TomKitchen screen from the main activity.

--- app/src/main/java/com/example/tomandjerry/MainActivity.kt
+++ app/src/main/java/com/example/tomandjerry/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import com.example.tomandjerry.screens.JerryStore
+import com.example.tomandjerry.screens.TomKitchen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,6 +26,7 @@ class MainActivity : ComponentActivity() {
                             .fillMaxSize()
                     ) {
                         //JerryStore()
+                        TomKitchen()
                     }
                 }
             )
