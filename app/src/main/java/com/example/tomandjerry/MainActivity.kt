23e90ae63ff3b1ad2526a043f61784e22ab44342
Thoraya <thoriahamdy99@gmail.com>
2025-06-05 13:37:58 +0300

    set the status bar padding only to the screen

--- app/src/main/java/com/example/tomandjerry/MainActivity.kt
+++ app/src/main/java/com/example/tomandjerry/MainActivity.kt
@@ -1,30 +1,30 @@
 package com.example.tomandjerry
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
-import com.example.tomandjerry.screens.JerryStore
 import com.example.tomandjerry.screens.TomAccount
-import com.example.tomandjerry.screens.TomKitchen
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             Scaffold(
                 modifier = Modifier.fillMaxSize(),
-                content = { innerPadding ->
+                content = {
                     Box(
                         modifier = Modifier
-                            .padding(innerPadding)
                             .fillMaxSize()
+                            .statusBarsPadding()
                     ) {
                         TomAccount()
                     }
